00fb04c069296a84c3933b9c7eb234df405fafc8
E-BAO <714667117@qq.com>
2019-04-16 14:58:47 -0700

    init

---
+++ build/CMakeFiles/DEMO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DEMO.dir/DEMO_generated_tsdf.cu.o"
+  "../lib/libDEMO.pdb"
+  "../lib/libDEMO.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/DEMO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
